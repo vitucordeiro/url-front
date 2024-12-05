17f58835ed1a92cf260f041913b9982db6c38437
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 15:21:20 -0300

    feat: react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 import './index.css'
+
 import App from './App.tsx'
 
 // src/main.jsx ou src/index.jsx
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/" element={<App />} />
+
+    </Routes>
+  </BrowserRouter>
 )
