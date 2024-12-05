cb30fd67bc5f2a99a39b9331c2eccdd700fe9bc9
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 17:41:48 -0300

    feat: add /share path

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 
 import App from './App.tsx'
+import Share from './pages/Share.tsx'
 
 // src/main.jsx ou src/index.jsx
 
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/" element={<App />} />
+      <Route path="/share" element={<Share />} />
 
     </Routes>
   </BrowserRouter>
