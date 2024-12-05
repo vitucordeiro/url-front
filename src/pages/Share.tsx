1ef277c9638ba4ce70a6270c86f47dc2436e2107
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 14:38:58 -0300

    feat: share

---
+++ src/pages/Share.tsx
@@ -0,0 +1,9 @@
+import Header from "../Components/Header";
+
+export default function Share() {
+  return (
+    <>
+      <Header H1="h1 to share" description="share description" />
+    </>
+  )
+}
