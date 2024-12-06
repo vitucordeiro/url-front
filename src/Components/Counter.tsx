44542be9c8c54df89a4ab5e6faec749cbc951ae4
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 21:42:53 -0300

    feat: Counter component

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./Counter.css"
+const Counter = () => {
+  const [count, SetCount] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:8080/count", {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json',
+        }
+      })
+      if (response.ok) {
+        const data = await response.json();
+        SetCount(data);
+      }
+      console.error("Error")
+    }
+    fetchData();
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+        <span className="span-counter">We currently shorten  {count} URL's! </span>
+      </div>
+    </>
+  )
+
+}
+
+export default Counter;
