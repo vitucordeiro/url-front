c3497d85d0045eece67c06821151355ba6c92bfe
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 21:43:08 -0300

    feat: Footer component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+const Footer = () => {
+  return (
+    <>
+      <div className="div-container">
+        <span className="">All rights reserved. created by Victor Cordeiro.</span>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
