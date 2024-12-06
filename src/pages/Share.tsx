0375b7485b1c32e4bce4d8082ba5b91b333d323a
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 21:43:43 -0300

    feat: Share page

--- src/pages/Share.tsx
+++ src/pages/Share.tsx
@@ -1,9 +1,17 @@
+import { useLocation } from "react-router";
 import Header from "../Components/Header";
+import Card from "../Components/Card";
+import Footer from "../Components/Footer";
 
 export default function Share() {
+  const location = useLocation();
+
   return (
     <>
-      <Header H1="h1 to share" description="share description" />
+      <Header H1="Simple URL Shortener" description="Share your shorten Url" />
+      <Card arrowToHome={false} />
+      <Footer />
     </>
+
   )
 }
