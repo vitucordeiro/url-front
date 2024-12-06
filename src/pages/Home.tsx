c4d5635604c44182d4e5a390bcc6900f1310ce2f
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 21:43:25 -0300

    feat: Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router";
 import Card from "../Components/Card";
 import Header from "../Components/Header";
-
+import './Home.css'
+import Footer from "../Components/Footer";
+import Counter from "../Components/Counter";
 export default function Home() {
   return (
     <>
       <Header H1="Simple URL Shortener" description="Shorten your links and track every click in real-time!" />
       <Card arrowToHome={true}></Card>
+      <Counter />
+      <Footer />
     </>
   )
 }
