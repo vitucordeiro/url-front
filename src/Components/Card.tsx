e1a73ead328185b867b15f8b930fbab47dd0f664
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-05 21:42:38 -0300

    feat: card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 import './Card.css';
-import { CircularProgress } from "@mui/material";
-import { useNavigate, useLocation } from "react-router";
 
+import ArrowBack from "@mui/icons-material/ArrowBack";
+import { CircularProgress, } from "@mui/material";
+import { useNavigate, useLocation, Link } from "react-router";
+import LaunchIcon from '@mui/icons-material/Launch';
 type CardProps = {
   arrowToHome: boolean
 }
@@ -14,12 +16,15 @@ const Card: React.FC<CardProps> = ({ arrowToHome }) => {
   const [error, SetError] = useState('');
   const [isLoading, SetIsLoading] = useState(false);
   const [shortenUrl, SetShortenUrl] = useState('');
+  const [isCopied, SetIsCopied] = useState(false);
 
+  const urlRegex = /^(https?:\/\/)?([a-z0-9-]+\.)+[a-z0-9]{2,}(\/[^\s]*)?$/i;
 
   async function fetchData(url: string) {
     SetIsLoading(true)
     SetError('');
 
+    //TODO: add env variable 
     const response = await fetch("http://localhost:8080/create", {
       method: 'POST',
       headers: {
@@ -35,44 +40,78 @@ const Card: React.FC<CardProps> = ({ arrowToHome }) => {
   function handlerSubmit(event: React.FormEvent<HTMLFormElement>) {
 
     event.preventDefault();
+    // Prevent wrong formats of URI before request 
+    if (!urlRegex.test(longUrl)) {
+      console.error("URL wrong format")
+      return
+    }
     // hook to request the shorten url and redirect user to /share
+
     fetchData(longUrl)
   }
 
-  if (arrowToHome) {
+  async function handlerCopy() {
+
+    navigator.clipboard.writeText(location.state?.shortUrl)
+    SetIsCopied(true)
+  }
+  function handlerArrow() {
+    navigation('/')
+  }
+  if (!arrowToHome) {
     return (
       <>
         <div className="card-container">
-          <div className="card-arrow-home"></div>
-          <form onSubmit={handlerSubmit} className="card-div-container">
+          <div className="card-arrow-home" onClick={handlerArrow}>
+            <ArrowBack className="arrow-left" />
+          </div>
 
-            <input
-              className="input-text"
-              type="text"
-              placeholder="your long url here"
-              value={longUrl}
-              onChange={(e) => { SetLongUrl(e.target.value) }}
-            />
-            {isLoading ? (
-              <CircularProgress />
-            ) : (
-              <button type="submit" className="button"> GENERATE</button>
-            )}
-          </form>
-
+          <div className="card-div-container-result" onClick={handlerCopy}>
+            {isCopied ? (
+              <span className="result-text" > COPIED!</span>
+            ) : location.state?.shortUrl ? (
+              <span className="result-text">{location.state?.shortUrl}</span>
+            ) :
+              (
+                <span className="result-text">First, give us a long Url. </span>
+              )}
+          </div>
+        </div >
+        <div className="div-description">
+          <span className="span">Track your new shorten Url
+            <Link to={'/track'} className="span-link">
+              <LaunchIcon />
+            </Link>
+          </span>
         </div>
       </>)
   }
   return (
+
+
     <>
       <div className="card-container">
-        <div className="card-div-container">
-          <span className="input-text">{location.state?.shortUrl}</span>
-        </div>
+        <form onSubmit={handlerSubmit} className="card-div-container">
 
-      </div >
-    </>
-  )
+          <input
+            className="input-text"
+            type="text"
+            placeholder="your long url here"
+            value={longUrl}
+            onChange={(e) => { SetLongUrl(e.target.value) }}
+          />
+          {isLoading ? (
+            <CircularProgress />
+          ) : (
+            <button type="submit" className="button"> GENERATE</button>
+          )}
+        </form>
+      </div>
+      <div className="div-description">
+        <span className="span">You already created?
+          <Link to={'/track'} className="span-link"> track</Link> your shorten URL.</span>
+      </div>
+    </>)
 }
 
 export default Card;
