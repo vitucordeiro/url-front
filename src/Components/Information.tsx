4634e002084bff318bf5bc09fd5a8a28ce9ae1d0
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-12-07 13:38:33 -0300

    add: information page

---
+++ src/Components/Information.tsx
@@ -0,0 +1,12 @@
+import './Information.css'
+const Information = () => {
+  return (
+    <>
+      <div className="container-information">
+        <span className="span-information"></span>
+      </div>
+    </>
+  )
+}
+
+export default Information;
