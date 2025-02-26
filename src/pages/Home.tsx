c374010c95bb235215a0245e41cbd2700a4d8ea3
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2025-02-26 14:38:16 -0300

    Delete Information component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,12 @@ import Header from "../Components/Header";
 import './Home.css'
 import Footer from "../Components/Footer";
 import Counter from "../Components/Counter";
-import Information from "../Components/Information";
 export default function Home() {
   return (
     <>
       <Header H1="Simple URL Shortener" description="Shorten your links and track every click in real-time!" />
       <Card arrowToHome={true}></Card>
       <Counter />
-      <Information />
       <Footer />
     </>
   )
